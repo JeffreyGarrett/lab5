499d18b4154deb5c72fc5eb4082c31ffe0478a00
= <jeff2invest@gmail.com>
2020-07-25 20:10:50 -0700

    adding files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const request = require("request");
+const pool = requirte("./dbPool.js");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
@@ -10,7 +11,7 @@ app.get("/", async function (req, res) {
 
     let imageUrlArray = await getRandomImage("", 1);
     res.render("index", { "imageUrlArray": imageUrlArray });
-    
+
 });
 
 app.get("/search", async function (req, res) {
@@ -27,6 +28,26 @@ app.get("/search", async function (req, res) {
 
 });
 
+app.get("/api/updateFavorites", function (req, res) {
+    let sql;
+    let sqlParams;
+    switch (req.query.action) {
+        case "add": sql = "INSERT INTO favorites (imageUrl, keyword) VALUES (?,?)";
+            sqlParams = [req.query.imageUrl, req.query.keyword];
+            break;
+        case "delete": sql = "DELETE FROM favorites WHERE imageUrl = ?";
+            sqlParams = [req.query.imageUrl];
+            break;
+    }//switch
+    pool.query(sql, sqlParams, function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        res.send(rows.affectedRows.toString());
+    });
+
+});//api/updateFavorites
+
+
 function getRandomImage(keyword, count) {
     return new Promise(function (resolve, reject) {
 
@@ -38,9 +59,9 @@ function getRandomImage(keyword, count) {
 
                 let imageUrlArray = [];
                 for (let i = 0; i < count; i++) {
-                    imageUrlArray.push(parsedData[i]["urls"]["regular"]);                 
+                    imageUrlArray.push(parsedData[i]["urls"]["regular"]);
                 }
-                
+
                 resolve(imageUrlArray);
             } else {
                 console.log('error:', error);
